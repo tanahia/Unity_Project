01708ee12ac2d04efe5b8f50285a94702d555ede
tanahia <sofi.bookfan@gmail.com>
2024-12-11 18:39:33 +0000

    Wrote script for character's movement Signed-off-by: tanahia <sofi.bookfan@gmail.com>

--- unity/Assets/cat/MovementScript.cs
+++ unity/Assets/cat/MovementScript.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class MovementScript : MonoBehaviour
+public class movementScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float turningSpeed = 90;
+    float WalkingSpeed = 550;
+    Animator playerAnimator;
+    Rigidbody rb;
+    float moveX;
+    float moveY;
+
+
     void Start()
     {
-        
+        playerAnimator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        playerAnimator.SetBool("isWalking", false);
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            playerAnimator.SetBool("isWalking", true);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            playerAnimator.SetBool("isWalking", true);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+
+
+            playerAnimator.SetBool("isWalking", true);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+
+
+            playerAnimator.SetBool("isWalking", true);
+
+        }
+        rb.velocity=(transform.forward*moveY)*WalkingSpeed*Time.deltaTime;
+        transform.Rotate((transform.up * moveX) * turningSpeed * Time.deltaTime);
+
     }
+
 }
+
